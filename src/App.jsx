2b56576b42321f27cec8aa4ce87d3baeb9ee9baa
triagusabdi <81340416+triagusabdi@users.noreply.github.com>
2023-10-29 17:16:34 +0700

    Profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Places from "./components/Home/Places";
+import Login from "./components/UserAccount/Login";
+import Map from "./components/InteractiveMapView/Map";
+// import SignUp from "./components/UserAccount/Signup";
+import Profile from "./components/UserAccount/Profile";
 
 function App() {
-  return <Places />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Places />} />
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/signup" element={<SignUp />} /> */}
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App;
+export default App;
