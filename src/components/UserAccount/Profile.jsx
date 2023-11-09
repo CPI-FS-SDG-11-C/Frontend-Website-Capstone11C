af8b108579d4b0c58f9668ebd9b3b93e4e47800a
Muhammad Arfani Asra <arfani152@gmail.com>
2023-11-10 06:05:14 +0800

    Fixing Profile page layout

--- src/components/UserAccount/Profile.jsx
+++ src/components/UserAccount/Profile.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Rating from '@mui/material/Rating';
 import Avatar from '@mui/material/Avatar';
+import { Link } from 'react-router-dom';
 
 
 function Profile() {
@@ -22,8 +23,8 @@ function Profile() {
             <h3>Profile</h3>
             <div className='userinfo'>
                 <div className="picture">
-                <Avatar alt="Remy Sharp" src="https://img.freepik.com/premium-vector/gray-avatar-icon-vector-illustration_276184-163.jpg" width="156" height="156" />
-                    <img src="https://img.freepik.com/premium-vector/gray-avatar-icon-vector-illustration_276184-163.jpg" alt="Profile Picture" width="156" height="156" viewBox="0 0 156 156" />        
+                    <Avatar alt="Remy Sharp" src="https://img.freepik.com/premium-vector/gray-avatar-icon-vector-illustration_276184-163.jpg" sx={{ width: 156, height: 156 }} />
+                    {/* <img src="https://img.freepik.com/premium-vector/gray-avatar-icon-vector-illustration_276184-163.jpg" alt="Profile Picture" width="156" height="156" viewBox="0 0 156 156" />         */}
                 </div>
                 <div className="urc">
                     <h2>username</h2>
@@ -39,9 +40,18 @@ function Profile() {
                 </div>
             </div>
             <div className='buttonsec'>
-                <button> Edit Profile </button>
+                <Link to="/edit" className="buttonsec">
+                    <Button variant="contained">Edit Profile</Button>
+                </Link>
+                <Link to="/changepassword" className="buttonsec">
+                    <Button variant="contained">Change Password</Button>
+                </Link>
+                <Link to="/login" className="buttonsec">
+                    <Button variant="contained" >Log Out</Button>
+                </Link>
+                {/* <button> Edit Profile </button>
                 <button> Change Password </button>
-                <button> Log Out </button>
+                <button> Log Out </button> */}
             </div>
             <div className='border-rating'>
                 <h3>Rating & Comment History</h3>
@@ -56,6 +66,29 @@ function Profile() {
                                 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
                                 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum 
                                 dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            </h5>
+                        </div>
+                    </li>
+                    <li>
+                        <div className='comment'>
+                            <h3>Nama RTH</h3>
+                            <h4>timestamp nya</h4>
+                            <Rating name="read-only" value={value} readOnly />
+                            <h5>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum 
+                                dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                                ullamco laboris nisi ut aliquip ex ea commodo consequat.
                                 labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
                                 ullamco laboris nisi ut aliquip ex ea commodo consequat.
                             </h5>
