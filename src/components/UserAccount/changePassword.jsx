029d25a5da16b59ac8cc5244372259f2870cce42
Muhammad Arfani Asra <arfani152@gmail.com>
2023-11-12 17:34:05 +0800

    Fix Profile, Edit Profile, Change Password responsiveness

--- src/components/UserAccount/changePassword.jsx
+++ src/components/UserAccount/changePassword.jsx
@@ -11,6 +11,7 @@ import InputAdornment from "@mui/material/InputAdornment";
 import IconButton from "@mui/material/IconButton";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { Container } from "@mui/material";
 
 function changePassword() {
   const [showPassword, setShowPassword] = React.useState(false);
@@ -23,96 +24,98 @@ function changePassword() {
 
   return (
     <>
-      <div className="main">
-        <div className="box">
-          <h3>Change Password</h3>
-          <Box
-            component="form"
-            sx={{
-              "& .MuiTextField-root": { m: 2, width: "80ch" },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <div className="inputedit">
-              <FormControl sx={{ m: 1, width: "80ch" }} variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-password">
-                  Current Password
-                </InputLabel>
-                <OutlinedInput
-                  id="outlined-adornment-password"
-                  type={showPassword ? "text" : "password"}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        edge="end"
-                      >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                  label="Current Password"
-                />
-              </FormControl>
-              <FormControl sx={{ m: 1, width: "80ch" }} variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-password">
-                  New Password
-                </InputLabel>
-                <OutlinedInput
-                  id="outlined-adornment-password"
-                  type={showPassword ? "text" : "password"}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        edge="end"
-                      >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                  label="New Password"
-                />
-              </FormControl>
-              <FormControl sx={{ m: 1, width: "80ch" }} variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-password">
-                  Confirm Password
-                </InputLabel>
-                <OutlinedInput
-                  id="outlined-adornment-password"
-                  type={showPassword ? "text" : "password"}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        edge="end"
-                      >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                  label="Confirm Password"
-                />
-              </FormControl>
-            </div>
-            <div className="buttonedit">
-              <Link to="/profile  " className="buttonedit">
-                <Button variant="contained">Back</Button>
-              </Link>
-              <Link to="/login" className="buttonedit">
-                <Button variant="contained">Save Change</Button>
-              </Link>
-            </div>
-          </Box>
+      <Container className="py-10 px-5">
+        <div className="main">
+          <div className="box">
+            <h3>Change Password</h3>
+            <Box
+              component="form"
+              sx={{
+                "& .MuiTextField-root": { m: 2, width: "80ch" },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <div className="inputedit">
+                <FormControl sx={{ m: 1, width: "80ch" }} variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-password">
+                    Current Password
+                  </InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showPassword ? "text" : "password"}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Current Password"
+                  />
+                </FormControl>
+                <FormControl sx={{ m: 1, width: "80ch" }} variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-password">
+                    New Password
+                  </InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showPassword ? "text" : "password"}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="New Password"
+                  />
+                </FormControl>
+                <FormControl sx={{ m: 1, width: "80ch" }} variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-password">
+                    Confirm Password
+                  </InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showPassword ? "text" : "password"}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Confirm Password"
+                  />
+                </FormControl>
+              </div>
+              <div className="buttonedit">
+                <Link to="/profile  " className="buttonedit">
+                  <Button variant="contained">Back</Button>
+                </Link>
+                <Link to="/login" className="buttonedit">
+                  <Button variant="contained">Save Change</Button>
+                </Link>
+              </div>
+            </Box>
+          </div>
         </div>
-      </div>
+      </Container>
     </>
   );
 }
